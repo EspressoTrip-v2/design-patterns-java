d358101a219750095de0dff91fefe6d10c9d06df
EspressoTrip-v2 <juanbo.jb@gmail.com>
2021-01-13 12:59:32 +0200

    Adapter Pattern

--- Structural Patterns/Adapter/AdapterChallenge/src/com/company/Client.java
+++ Structural Patterns/Adapter/AdapterChallenge/src/com/company/Client.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Main {
+public class Client {
 
     public static void main(String[] args) {
 
@@ -8,13 +8,14 @@ public class Main {
         audioPlayer.play("mp3", "Juan_Music.mp3");
 
         AdvancedMediaPlayerInterface mp4Player = new Mp4();
-        MediaPlayerInterface advancedMediaPlayerAdapter1 = new AdvancedMediaPlayerAdapter(mp4Player);
+        MediaPlayerInterface advancedMediaPlayerAdapter1 =
+                new AdvancedMediaPlayerAdapter(mp4Player);
         advancedMediaPlayerAdapter1.play("mp4", "Alone.mp4");
 
         AdvancedMediaPlayerInterface vlcPlayer = new Vlc();
-        MediaPlayerInterface advancedMediaPlayerAdapter2 = new AdvancedMediaPlayerAdapter(vlcPlayer);
+        MediaPlayerInterface advancedMediaPlayerAdapter2 =
+                new AdvancedMediaPlayerAdapter(vlcPlayer);
         advancedMediaPlayerAdapter2.play("vlc", "Its_all_about_you.vlc");
-
 
     }
 }
